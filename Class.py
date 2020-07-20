21d967aa2592e09c206b939e6e18acc5eb3d5a1f
K <ways2kim@gmail.com>
2020-07-21 03:30:12 +0900

    함수

---
+++ Class.py
@@ -0,0 +1,15 @@
+# 파이썬 클래스 상세 이해
+# Self, 클래스, 인스턴스 변수
+
+# 예제1
+
+
+class UserInfo:
+    # 속성, 메소드
+    def __init__(self):
+        self.name = name
+    def user_info_p(self):
+        print("Name : ", self.name)
+
+
+user1 = UserInfo()
